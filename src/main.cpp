e724377b6cc46757d221be07e908e8373af1ace5
Simon Ser <contact@emersion.fr>
2021-04-16 12:24:27 +0200

    Revert "main: set R600_DEBUG=nodcc ourselves if we have a sub-command."
    This reverts commit 11dd8014ad8de6a3ab0c1cdb7d307d97d22d3b1a.
    
    GFX9+ now supports modifiers. Requires Mesa 21.1+ (for
    VK_EXT_image_drm_format_modifier).

--- src/main.cpp
+++ src/main.cpp
@@ -112,23 +112,6 @@ int main(int argc, char **argv)
 		}
 	}
 
-	// If we're going to execute something monolithic, might as well set this since it'll extend to our clients
-	if ( g_nSubCommandArg != 0 )
-	{
-		const char *pchR600Debug = getenv( "R600_DEBUG" );
-
-		if ( pchR600Debug == nullptr )
-		{
-			setenv( "R600_DEBUG", "nodcc", 1 );
-		}
-		else if ( strstr( pchR600Debug, "nodcc" ) == nullptr )
-		{
-			std::string strPreviousR600Debug = pchR600Debug;
-			strPreviousR600Debug.append( ",nodcc" );
-			setenv( "R600_DEBUG", strPreviousR600Debug.c_str(), 1 );
-		}
-	}
-
 	cap_t caps;
 	caps = cap_get_proc();
 	cap_flag_value_t nicecapvalue = CAP_CLEAR;
