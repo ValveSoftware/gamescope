9cde9eb17a27f112b09e7d560588b914fc9562ea
Fabian Arndt <fabian.arndt@root-core.net>
2023-06-07 17:17:54 +0200

    Modified MangoApp protocol to not repurpose / rename fields

--- src/mangoapp.cpp
+++ src/mangoapp.cpp
@@ -19,13 +19,15 @@ struct mangoapp_msg_v1 {
 
     uint32_t pid;
     uint64_t visible_frametime_ns;
-    uint8_t scaler_filter;
-    uint8_t fsrSharpness;
+    uint8_t fsrUpscale;     // deprecated
+    uint8_t fsrSharpness;   // deprecated
     uint64_t app_frametime_ns;
     uint64_t latency_ns;
     uint32_t outputWidth;
     uint32_t outputHeight;
     uint8_t scaler;
+    uint8_t scaler_filter;
+    uint8_t scaler_sharpness;
     // WARNING: Always ADD fields, never remove or repurpose fields
 } __attribute__((packed)) mangoapp_msg_v1;
 
@@ -34,7 +36,7 @@ void init_mangoapp(){
     msgid = msgget(key, 0666 | IPC_CREAT);
     mangoapp_msg_v1.hdr.msg_type = 1;
     mangoapp_msg_v1.hdr.version = 1;
-    mangoapp_msg_v1.scaler_filter = 0;
+    mangoapp_msg_v1.fsrUpscale = 0;
     mangoapp_msg_v1.fsrSharpness = 0;
     inited = true;
 }
@@ -44,7 +46,7 @@ void mangoapp_update( uint64_t visible_frametime, uint64_t app_frametime_ns, uin
         init_mangoapp();
 
     mangoapp_msg_v1.visible_frametime_ns = visible_frametime;
-    mangoapp_msg_v1.scaler_filter = (uint8_t)g_upscaleFilter;
+    mangoapp_msg_v1.fsrUpscale = g_bFSRActive;
     mangoapp_msg_v1.fsrSharpness = g_upscaleFilterSharpness;
     mangoapp_msg_v1.app_frametime_ns = app_frametime_ns;
     mangoapp_msg_v1.latency_ns = latency_ns;
@@ -52,5 +54,7 @@ void mangoapp_update( uint64_t visible_frametime, uint64_t app_frametime_ns, uin
     mangoapp_msg_v1.outputWidth = g_nOutputWidth;
     mangoapp_msg_v1.outputHeight = g_nOutputHeight;
     mangoapp_msg_v1.scaler = (uint8_t)g_upscaleScaler;
+    mangoapp_msg_v1.scaler_filter = (uint8_t)g_upscaleFilter;
+    mangoapp_msg_v1.scaler_sharpness = g_upscaleFilterSharpness;
     msgsnd(msgid, &mangoapp_msg_v1, sizeof(mangoapp_msg_v1) - sizeof(mangoapp_msg_v1.hdr.msg_type), IPC_NOWAIT);
 }
