07b3823f809d59a5b4b68d6f04e03f91aa6ce7c2
Alex Maese <memaese@hotmail.com>
2023-07-07 15:16:34 -0500

    Fix build issue
    Fixes the following error:
    error: control reaches end of non-void function [-Werror=return-type]

--- src/main.cpp
+++ src/main.cpp
@@ -847,13 +847,7 @@ static bool initOutput( int preferredWidth, int preferredHeight, int preferredRe
 		{
 			return sdlwindow_init();
 		}
-		else
-		{
-			return true;
-		}
+		return true;
 	}
-	else
-	{
-		return init_drm( &g_DRM, preferredWidth, preferredHeight, preferredRefresh, s_bInitialWantsVRREnabled );
-	}
+	return init_drm( &g_DRM, preferredWidth, preferredHeight, preferredRefresh, s_bInitialWantsVRREnabled );
 }
