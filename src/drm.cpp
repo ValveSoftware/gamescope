ValveSoftware/gamescope

commit e810317e9507573a2ce7164b0e3bc3b2171b3052

steamcompmgr, drm: Add support for disjoint content and output encoding colorspaces

src/drm.cpp
@@ -2611,10 +2611,18 @@ bool drm_supports_color_mgmt(struct drm_t *drm)
 	return true;
 }
 
-const displaycolorimetry_t &drm_get_native_colorimetry(struct drm_t *drm)
+void drm_get_native_colorimetry(struct drm_t *drm, displaycolorimetry_t *displayColorimetry, displaycolorimetry_t *outputEncodingColorimetry)
 {
 	if ( !drm || !drm->connector )
-		return displaycolorimetry_709_gamma22;
+	{
+		*displayColorimetry = displaycolorimetry_709_gamma22;
+		*outputEncodingColorimetry = displaycolorimetry_709_gamma22;
+	}
 
-	return drm->connector->metadata.colorimetry;
+	*displayColorimetry = drm->connector->metadata.colorimetry;
+	// For HDR output, expected content colorspace != native colorspace.
+	if (drm->connector->metadata.supportsST2084 && g_bOutputHDREnabled)
+		*outputEncodingColorimetry = displaycolorimetry_2020_pq;
+	else
+		*outputEncodingColorimetry = drm->connector->metadata.colorimetry;
 }

src/drm.hpp
@@ -319,7 +319,7 @@ const char *drm_get_device_name(struct drm_t *drm);
 std::pair<uint32_t, uint32_t> drm_get_connector_identifier(struct drm_t *drm);
 void drm_set_hdr_state(struct drm_t *drm, bool enabled);
 
-const displaycolorimetry_t &drm_get_native_colorimetry(struct drm_t *drm);
+void drm_get_native_colorimetry(struct drm_t *drm, displaycolorimetry_t *displayColorimetry, displaycolorimetry_t *outputEncodingColorimetry);
 
 extern bool g_bSupportsAsyncFlips;
 

src/rendervulkan.hpp
@@ -345,7 +345,13 @@ struct gamescope_color_mgmt_t
 	uint32_t externalDirtyCtr;
 	nightmode_t nightmode;
 	float sdrGamutWideness; // user property to widen gamut
-	displaycolorimetry_t nativeDisplayColorimetry;
+
+	// the native colorimetry capabilities of the display
+	displaycolorimetry_t displayColorimetry;
+	// the output encoding colorimetry
+	// ie. for HDR displays we send an explicit 2020 colorimetry packet.
+	// on SDR displays this is the same as displayColorimetry.
+	displaycolorimetry_t outputEncodingColorimetry;
 
 	bool operator <=> (const gamescope_color_mgmt_t&) const = default;
 };

src/steamcompmgr.cpp
@@ -112,11 +112,12 @@ update_color_mgmt()
 	// update pending native display colorimetry
 	if ( !BIsNested() )
 	{
-		g_ColorMgmt.pending.nativeDisplayColorimetry = drm_get_native_colorimetry( &g_DRM );
+		 drm_get_native_colorimetry( &g_DRM, &g_ColorMgmt.pending.displayColorimetry, &g_ColorMgmt.pending.outputEncodingColorimetry );
 	}
 	else
 	{
-		g_ColorMgmt.pending.nativeDisplayColorimetry = displaycolorimetry_709_gamma22;
+		g_ColorMgmt.pending.displayColorimetry = displaycolorimetry_709_gamma22;
+		g_ColorMgmt.pending.outputEncodingColorimetry = displaycolorimetry_709_gamma22;
 	}
 
 	// check if any part of our color mgmt stack is dirty
@@ -125,7 +126,9 @@ update_color_mgmt()
 
 	if (g_ColorMgmt.pending.enabled)
 	{
-		const displaycolorimetry_t& nativeDisplayOutput = g_ColorMgmt.pending.nativeDisplayColorimetry;
+		const displaycolorimetry_t& displayColorimetry = g_ColorMgmt.pending.displayColorimetry;
+		const displaycolorimetry_t& outputEncodingColorimetry = g_ColorMgmt.pending.outputEncodingColorimetry;
+
 		const float flSDRGamutWideness = g_ColorMgmt.pending.sdrGamutWideness;
 
 		std::vector<uint16_t> lut3d;
@@ -151,14 +154,14 @@ update_color_mgmt()
 			EOTF planeEOTF = static_cast<EOTF>( i );
 			if ( planeEOTF == EOTF::Gamma22 )
 			{
-				buildSDRColorimetry( &inputColorimetry, &colorMapping, flSDRGamutWideness, nativeDisplayOutput );
+				buildSDRColorimetry( &inputColorimetry, &colorMapping, flSDRGamutWideness, displayColorimetry );
 			}
 			else if ( planeEOTF == EOTF::PQ )
 			{
-				buildPQColorimetry( &inputColorimetry, &colorMapping, nativeDisplayOutput );
+				buildPQColorimetry( &inputColorimetry, &colorMapping, displayColorimetry );
 			}
 
-			calcColorTransform( &lut1d[0], nLutSize1d, &lut3d[0], nLutEdgeSize3d, inputColorimetry, nativeDisplayOutput,
+			calcColorTransform( &lut1d[0], nLutSize1d, &lut3d[0], nLutEdgeSize3d, inputColorimetry, outputEncodingColorimetry,
 				colorMapping, g_ColorMgmt.pending.nightmode, tonemapping );
 
 			if ( !g_ColorMgmtLutsOverride[i].lut3d.empty() && !g_ColorMgmtLutsOverride[i].lut3d.empty() )

commit 92fafa5c12439180a7d999d770d7f21912a3a240

drm: fix NPE while in headless mode
caused by e810317

src/drm.cpp
@@ -3232,6 +3232,7 @@ void drm_get_native_colorimetry( struct drm_t *drm,
 		*displayEOTF = EOTF_Gamma22;
 		*outputEncodingColorimetry = displaycolorimetry_709;
 		*outputEncodingEOTF = EOTF_Gamma22;
+		return;
 	}
 
 	*displayColorimetry = drm->connector->metadata.colorimetry;