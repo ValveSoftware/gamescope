e8b5a00b810f080aafb3b401c7e656957dfe695e
davidawesome02-backup <138835606+davidawesome02-backup@users.noreply.github.com>
2025-07-12 14:51:20 -0700

    Fix unfinished comment change, reorder cursor composite checks to make gamescopectl's overrides work properly again.

--- src/steamcompmgr.cpp
+++ src/steamcompmgr.cpp
@@ -2394,10 +2394,6 @@ static void paint_pipewire()
 gamescope::ConVar<int> cv_cursor_composite{ "cursor_composite", 1, "0 = Never composite a cursor. 1 = Composite cursor when not nested. 2 = Always composite a cursor manually" };
 bool ShouldDrawCursor()
 {
-	if (g_libinputSelectedDevices.size() > 0 && g_bMouseDisabled) {
-		return true;
-	}
-
 	if (cv_cursor_composite == 2 )
 		return true;
 
@@ -2406,6 +2402,10 @@ bool ShouldDrawCursor()
 
 	if ( g_bForceRelativeMouse )
 		return true;
+
+	if (g_libinputSelectedDevices.size() > 0 && g_bMouseDisabled) {
+		return true;
+	}
 
 	global_focus_t *pFocus = GetCurrentFocus();
 	if ( !pFocus )
