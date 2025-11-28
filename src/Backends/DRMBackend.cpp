ValveSoftware/gamescope

commit 8f82d515384026bb4d4b1678843dc0cb9a233052

backend: Add support for deferred backends
So we can eg. init Gamescope headlessly then transition into a backend that only uses SAMPLED modifiers like SteamVR.

This way we can init VRWebHelper in a Gamescope before SteamVR fully comes up.

src/Backends/DRMBackend.cpp
@@ -1523,7 +1523,7 @@ void finish_drm(struct drm_t *drm)
 	// page-flip handler thread.
 }
 
-gamescope::OwningRc<gamescope::IBackendFb> drm_fbid_from_dmabuf( struct drm_t *drm, struct wlr_buffer *buf, struct wlr_dmabuf_attributes *dma_buf )
+gamescope::OwningRc<gamescope::IBackendFb> drm_fbid_from_dmabuf( struct drm_t *drm, struct wlr_dmabuf_attributes *dma_buf )
 {
 	gamescope::OwningRc<gamescope::IBackendFb> pBackendFb;
 	uint32_t fb_id = 0;
@@ -2525,7 +2525,7 @@ drm_prepare_liftoff( struct drm_t *drm, const struct FrameInfo_t *frameInfo, boo
 		if ( i < frameInfo->layerCount )
 		{
 			const FrameInfo_t::Layer_t *pLayer = &frameInfo->layers[ i ];
-			gamescope::CDRMFb *pDrmFb = static_cast<gamescope::CDRMFb *>( pLayer->tex ? pLayer->tex->GetBackendFb() : nullptr );
+			gamescope::CDRMFb *pDrmFb = static_cast<gamescope::CDRMFb *>( pLayer->tex ? pLayer->tex->GetBackendFb()->Unwrap() : nullptr );
 
 			if ( pDrmFb == nullptr )
 			{
@@ -3710,9 +3710,9 @@ namespace gamescope
 			return std::make_shared<BackendBlob>( data, uBlob, true );
 		}
 
-		virtual OwningRc<IBackendFb> ImportDmabufToBackend( wlr_buffer *pBuffer, wlr_dmabuf_attributes *pDmaBuf ) override
+		virtual OwningRc<IBackendFb> ImportDmabufToBackend( wlr_dmabuf_attributes *pDmaBuf ) override
 		{
-			return drm_fbid_from_dmabuf( &g_DRM, pBuffer, pDmaBuf );
+			return drm_fbid_from_dmabuf( &g_DRM, pDmaBuf );
 		}
 
 		virtual bool UsesModifiers() const override

src/Backends/DeferredBackend.h
@@ -1,0 +1,432 @@
+#include "backend.h"
+#include "refresh_rate.h"
+#include "steamcompmgr.hpp"
+
+#include <cassert>
+#include <shared_mutex>
+
+extern int g_nPreferredOutputWidth;
+extern int g_nPreferredOutputHeight;
+
+std::span<const uint64_t> GetSupportedSampleModifiers( uint32_t uDrmFormat );
+
+namespace gamescope
+{
+    class CDeferredBackend;
+
+    class CDeferredFb final : public CBaseBackendFb
+    {
+    public:
+        CDeferredFb( CDeferredBackend *pDeferredBackend, struct wlr_dmabuf_attributes *attributes )
+            : m_pDeferredBackend{ pDeferredBackend }
+        {
+            wlr_dmabuf_attributes_copy( &m_attributes, attributes );
+        }
+
+        ~CDeferredFb()
+        {
+            wlr_dmabuf_attributes_finish( &m_attributes );
+        }
+        
+        IBackendFb *Unwrap() override;
+    private:
+        CDeferredBackend *m_pDeferredBackend = nullptr;
+        struct wlr_dmabuf_attributes m_attributes;
+        OwningRc<IBackendFb> m_pChild;
+    };
+
+	class CDeferredBackend final : public CBaseBackend
+	{
+	public:
+		CDeferredBackend( IBackend *pChild )
+            : m_pChild{ pChild }
+		{
+		}
+
+		virtual ~CDeferredBackend()
+		{
+            if ( m_pChild )
+            {
+                delete m_pChild;
+                m_pChild = nullptr;
+            }
+		}
+
+		virtual bool Init() override
+		{
+			g_nOutputWidth = g_nPreferredOutputWidth;
+			g_nOutputHeight = g_nPreferredOutputHeight;
+			g_nOutputRefresh = g_nNestedRefresh;
+
+			if ( g_nOutputHeight == 0 )
+			{
+				if ( g_nOutputWidth != 0 )
+				{
+					fprintf( stderr, "Cannot specify -W without -H\n" );
+					return false;
+				}
+				g_nOutputHeight = 720;
+			}
+			if ( g_nOutputWidth == 0 )
+				g_nOutputWidth = g_nOutputHeight * 16 / 9;
+			if ( g_nOutputRefresh == 0 )
+				g_nOutputRefresh = ConvertHztomHz( 60 );
+
+            if ( !vulkan_init( vulkan_get_instance(), VK_NULL_HANDLE ) )
+            {
+                return false;
+            }
+
+            if ( !wlsession_init() )
+            {
+                fprintf( stderr, "Failed to initialize deferred backend\n" );
+                return false;
+            }
+
+            TryInittingChild();
+
+			return true;
+		}
+
+		virtual bool PostInit() override
+		{
+            {
+                std::shared_lock lock{ m_mutInit };
+                m_bDonePostInit = true;
+
+                if ( m_bInittedChild )
+                    return m_pChild->PostInit();
+            }
+
+            return true;
+		}
+
+        virtual std::span<const char *const> GetInstanceExtensions() const override
+		{
+            // Basically what's needed to support SDL + OpenVR.
+            static const std::array<const char *const, 8> requiredInstanceExts
+            {
+                VK_KHR_EXTERNAL_MEMORY_CAPABILITIES_EXTENSION_NAME,
+                VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME,
+                VK_KHR_EXTERNAL_FENCE_CAPABILITIES_EXTENSION_NAME,
+                VK_KHR_SURFACE_EXTENSION_NAME,
+                "VK_KHR_xcb_surface",
+                "VK_KHR_xlib_surface",
+                "VK_KHR_wayland_surface",
+                VK_KHR_EXTERNAL_SEMAPHORE_CAPABILITIES_EXTENSION_NAME,
+            };
+			return std::span<const char *const>{ requiredInstanceExts };
+		}
+        virtual std::span<const char *const> GetDeviceExtensions( VkPhysicalDevice pVkPhysicalDevice ) const override
+		{
+            // Basically what's needed to support OpenVR.
+            static const std::array<const char *const, 8> requiredDeviceExts
+            {
+                VK_KHR_EXTERNAL_MEMORY_EXTENSION_NAME,
+                VK_KHR_EXTERNAL_SEMAPHORE_EXTENSION_NAME,
+                VK_KHR_TIMELINE_SEMAPHORE_EXTENSION_NAME,
+                VK_KHR_DEDICATED_ALLOCATION_EXTENSION_NAME,
+                VK_KHR_GET_MEMORY_REQUIREMENTS_2_EXTENSION_NAME,
+                VK_KHR_EXTERNAL_MEMORY_FD_EXTENSION_NAME,
+                VK_KHR_EXTERNAL_SEMAPHORE_FD_EXTENSION_NAME,
+                VK_KHR_IMAGE_FORMAT_LIST_EXTENSION_NAME,
+            };
+			return std::span<const char *const>{ requiredDeviceExts };
+		}
+        virtual VkImageLayout GetPresentLayout() const override
+		{
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->GetPresentLayout();
+            }
+
+			return VK_IMAGE_LAYOUT_GENERAL;
+		}
+		virtual void GetPreferredOutputFormat( uint32_t *pPrimaryPlaneFormat, uint32_t *pOverlayPlaneFormat ) const override
+		{
+			*pPrimaryPlaneFormat = VulkanFormatToDRM( VK_FORMAT_A2B10G10R10_UNORM_PACK32 );
+			*pOverlayPlaneFormat = VulkanFormatToDRM( VK_FORMAT_B8G8R8A8_UNORM );
+		}
+		virtual bool ValidPhysicalDevice( VkPhysicalDevice pVkPhysicalDevice ) const override
+		{
+			return true;
+		}
+
+		virtual void DirtyState( bool bForce, bool bForceModeset ) override
+		{
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->DirtyState( bForce, bForceModeset );
+            }
+		}
+
+		virtual bool PollState() override
+		{
+            TryInittingChild();
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->PollState() || m_bJustInittedPoll;
+            }
+			return false;
+		}
+
+		virtual std::shared_ptr<BackendBlob> CreateBackendBlob( const std::type_info &type, std::span<const uint8_t> data ) override
+		{
+            // Only dummy backend blobs supported.
+			return std::make_shared<BackendBlob>( data );
+		}
+
+		virtual OwningRc<IBackendFb> ImportDmabufToBackend( wlr_dmabuf_attributes *pDmaBuf ) override
+		{
+			return new CDeferredFb( this, pDmaBuf );
+		}
+
+		virtual bool UsesModifiers() const override
+		{
+            return true;
+		}
+		virtual std::span<const uint64_t> GetSupportedModifiers( uint32_t uDrmFormat ) const override
+		{
+			return GetSupportedSampleModifiers( uDrmFormat );
+		}
+
+		virtual IBackendConnector *GetCurrentConnector() override
+		{
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->GetCurrentConnector();
+            }
+
+            return nullptr;
+		}
+		virtual IBackendConnector *GetConnector( GamescopeScreenType eScreenType ) override
+		{
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->GetConnector( eScreenType );
+            }
+            
+			return nullptr;
+		}
+
+		virtual bool SupportsPlaneHardwareCursor() const override
+		{
+            // Doesn't need to be 'initted' for this check.
+            return m_pChild->SupportsPlaneHardwareCursor();
+		}
+
+		virtual bool SupportsTearing() const override
+		{
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->SupportsTearing();
+            }
+            
+			return false;
+		}
+
+		virtual bool UsesVulkanSwapchain() const override
+		{
+            // Doesn't need to be 'initted' for this check.
+            return m_pChild->UsesVulkanSwapchain();
+		}
+
+        virtual bool IsSessionBased() const override
+		{
+            // Doesn't need to be 'initted' for this check.
+            return m_pChild->IsSessionBased();
+		}
+
+		virtual bool SupportsExplicitSync() const override
+		{
+            // Doesn't need to be 'initted' for this check.
+            return m_pChild->SupportsExplicitSync();
+		}
+
+		virtual bool IsPaused() const override
+		{
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->IsPaused();
+            }
+
+            // We are always "paused" when not initted.
+            // Don't do any commits!
+			return true;
+		}
+
+		virtual bool IsVisible() const override
+		{
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->IsVisible();
+            }
+
+			return true;
+		}
+
+		virtual glm::uvec2 CursorSurfaceSize( glm::uvec2 uvecSize ) const override
+		{
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->CursorSurfaceSize( uvecSize );
+            }
+
+			return uvecSize;
+		}
+
+		virtual bool HackTemporarySetDynamicRefresh( int nRefresh ) override
+		{
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->HackTemporarySetDynamicRefresh( nRefresh );
+            }
+
+			return false;
+		}
+
+		virtual void HackUpdatePatchedEdid() override
+		{
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->HackUpdatePatchedEdid();
+            }
+		}
+
+        virtual bool NeedsFrameSync() const override
+        {
+            // Deferred backends do not support frame sync.
+            return false;
+        }
+
+        virtual TouchClickMode GetTouchClickMode() override
+        {
+            // Doesn't need to be 'initted' for this check.
+            return m_pChild->GetTouchClickMode();
+        }
+
+        virtual void DumpDebugInfo() override
+        {
+            // Doesn't need to be 'initted' for this check.
+            return m_pChild->DumpDebugInfo();
+        }
+
+        virtual bool UsesVirtualConnectors() override
+        {
+            // Doesn't need to be 'initted' for this check.
+            return m_pChild->UsesVirtualConnectors();
+        }
+        virtual std::shared_ptr<IBackendConnector> CreateVirtualConnector( uint64_t ulVirtualConnectorKey ) override
+        {
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->CreateVirtualConnector( ulVirtualConnectorKey );
+            }
+
+            return nullptr;
+        }
+
+        virtual void NotifyPhysicalInput( InputType eInputType ) override
+        {
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->NotifyPhysicalInput( eInputType );
+            }
+        }
+
+        virtual bool SupportsVROverlayForwarding() override
+        {
+            // Doesn't need to be 'initted' for this check.
+            return m_pChild->SupportsVROverlayForwarding();
+        }
+        virtual void ForwardFramebuffer( std::shared_ptr<IBackendPlane> &pPlane, IBackendFb *pFramebuffer, const void *pData ) override
+        {
+            {
+                std::shared_lock lock{ m_mutInit };
+                if ( m_bInittedChild )
+                    return m_pChild->ForwardFramebuffer( pPlane, pFramebuffer, pData );
+            }
+        }
+
+        bool IsChildInitted()
+        {
+            return m_bInittedChild;
+        }
+
+        IBackend *GetChild()
+        {
+            return m_pChild;
+        }
+
+        bool NewlyInitted() override
+        {
+            return m_bJustInittedClient.exchange( false );
+        }
+
+	protected:
+
+		virtual void OnBackendBlobDestroyed( BackendBlob *pBlob ) override
+		{
+		}
+
+	private:
+
+        void TryInittingChild()
+        {
+            if ( !m_bInittedChild )
+            {
+                std::unique_lock lock{ m_mutInit };
+                if ( !m_bInittedChild )
+                {
+                    if ( m_pChild->Init() )
+                    {
+                        m_bInittedChild = true;
+
+                        if ( m_bDonePostInit )
+                        {
+                            bool bRet = m_pChild->PostInit();
+                            assert( bRet && "PostInit failed!" );
+                        }
+
+                        m_bJustInittedClient = true;
+                        m_bJustInittedPoll = true;
+                    }
+                }
+            }
+        }
+
+        IBackend *m_pChild = nullptr;
+        mutable std::shared_mutex m_mutInit;
+        bool m_bDonePostInit = false;
+
+        std::atomic<bool> m_bInittedChild = { false };
+        std::atomic<bool> m_bJustInittedClient = { false };
+        std::atomic<bool> m_bJustInittedPoll = { false };
+	};
+
+    IBackendFb *CDeferredFb::Unwrap()
+    {
+        assert( m_pDeferredBackend->IsChildInitted() );
+
+        if ( !m_pChild )
+        {
+            m_pChild = m_pDeferredBackend->GetChild()->ImportDmabufToBackend( &m_attributes );
+        }
+
+        return m_pChild.get();
+    }
+
+}

src/Backends/HeadlessBackend.cpp
@@ -180,7 +180,7 @@ namespace gamescope
 			return std::make_shared<BackendBlob>( data );
 		}
 
-		virtual OwningRc<IBackendFb> ImportDmabufToBackend( wlr_buffer *pBuffer, wlr_dmabuf_attributes *pDmaBuf ) override
+		virtual OwningRc<IBackendFb> ImportDmabufToBackend( wlr_dmabuf_attributes *pDmaBuf ) override
 		{
 			return new CBaseBackendFb();
 		}

src/Backends/OpenVRBackend.cpp
@@ -48,6 +48,7 @@ extern std::string g_reshade_effect;
 extern gamescope::ConVar<bool> cv_hdr_enabled;
 
 extern uint64_t g_SteamCompMgrLimitedAppRefreshCycle;
+extern bool g_bAllowDeferredBackend;
 
 void MakeFocusDirty();
 void update_connector_display_info_wl(struct drm_t *drm);
@@ -553,33 +554,37 @@ namespace gamescope
                 m_pchOverlayName = "Gamescope";
 
             m_pIPCResourceManager = vr::VRIPCResourceManager();
-            if ( m_pIPCResourceManager )
+
+            if ( !g_bAllowDeferredBackend )
             {
-                uint32_t uFormatCount = 0;
-                m_pIPCResourceManager->GetDmabufFormats( &uFormatCount, nullptr );
-
-                if ( uFormatCount )
+                if ( m_pIPCResourceManager )
                 {
-                    std::vector<uint32_t> uFormats;
-                    uFormats.resize( uFormatCount );
-                    m_pIPCResourceManager->GetDmabufFormats( &uFormatCount, uFormats.data() );
+                    uint32_t uFormatCount = 0;
+                    m_pIPCResourceManager->GetDmabufFormats( &uFormatCount, nullptr );
 
-                    for ( uint32_t i = 0; i < uFormatCount; i++ )
+                    if ( uFormatCount )
                     {
-                        uint32_t uFormat = uFormats[i];
-                        uint32_t uModifierCount = 0;
-                        m_pIPCResourceManager->GetDmabufModifiers( vr::VRApplication_Overlay, uFormat, &uModifierCount, nullptr );
+                        std::vector<uint32_t> uFormats;
+                        uFormats.resize( uFormatCount );
+                        m_pIPCResourceManager->GetDmabufFormats( &uFormatCount, uFormats.data() );
 
-                        if ( uModifierCount )
+                        for ( uint32_t i = 0; i < uFormatCount; i++ )
                         {
-                            std::vector<uint64_t> ulModifiers;
-                            ulModifiers.resize( uModifierCount );
-                            m_pIPCResourceManager->GetDmabufModifiers( vr::VRApplication_Overlay, uFormat, &uModifierCount, ulModifiers.data() );
+                            uint32_t uFormat = uFormats[i];
+                            uint32_t uModifierCount = 0;
+                            m_pIPCResourceManager->GetDmabufModifiers( vr::VRApplication_Overlay, uFormat, &uModifierCount, nullptr );
 
-                            for ( uint64_t ulModifier : ulModifiers )
+                            if ( uModifierCount )
                             {
-                                if ( ulModifier != DRM_FORMAT_MOD_INVALID )
-                                    m_FormatModifiers[uFormat].emplace_back( ulModifier );
+                                std::vector<uint64_t> ulModifiers;
+                                ulModifiers.resize( uModifierCount );
+                                m_pIPCResourceManager->GetDmabufModifiers( vr::VRApplication_Overlay, uFormat, &uModifierCount, ulModifiers.data() );
+
+                                for ( uint64_t ulModifier : ulModifiers )
+                                {
+                                    if ( ulModifier != DRM_FORMAT_MOD_INVALID )
+                                        m_FormatModifiers[uFormat].emplace_back( ulModifier );
+                                }
                             }
                         }
                     }
@@ -677,7 +682,7 @@ namespace gamescope
 			return std::make_shared<BackendBlob>( data );
 		}
 
-		virtual OwningRc<IBackendFb> ImportDmabufToBackend( wlr_buffer *pBuffer, wlr_dmabuf_attributes *pDmaBuf ) override
+		virtual OwningRc<IBackendFb> ImportDmabufToBackend( wlr_dmabuf_attributes *pDmaBuf ) override
 		{
             if ( UsesModifiers() )
             {
@@ -738,6 +743,9 @@ namespace gamescope
 		{
             if ( !cv_vr_use_modifiers )
                 return false;
+
+            if ( g_bAllowDeferredBackend )
+                return true;
 
             if ( !m_pIPCResourceManager )
                 return false;
@@ -1834,7 +1842,7 @@ namespace gamescope
                     vr::VROverlay()->ShowOverlay( m_hOverlay );
                 }
 
-                pFb = static_cast<COpenVRFb *>( oState->pTexture->GetBackendFb() );
+                pFb = static_cast<COpenVRFb *>( oState->pTexture->GetBackendFb()->Unwrap() );
                 vr::SharedTextureHandle_t ulHandle = pFb->GetSharedTextureHandle();
 
                 vr::Texture_t texture = { (void *)&ulHandle, vr::TextureType_SharedTextureHandle, vr::ColorSpace_Gamma };

src/Backends/SDLBackend.cpp
@@ -55,10 +55,12 @@ namespace gamescope
 		GAMESCOPE_SDL_EVENT_COUNT,
 	};
 
+	class CSDLBackend;
+
 	class CSDLConnector final : public CBaseBackendConnector, public INestedHints
 	{
 	public:
-		CSDLConnector();
+		CSDLConnector( CSDLBackend *pBackend );
 		virtual bool Init();
 
 		virtual ~CSDLConnector();
@@ -121,6 +123,7 @@ namespace gamescope
 		SDL_Window *GetSDLWindow() const { return m_pWindow; }
 		VkSurfaceKHR GetVulkanSurface() const { return m_pVkSurface; }
 	private:
+		CSDLBackend *m_pBackend = nullptr;
 		SDL_Window *m_pWindow = nullptr;
 		VkSurfaceKHR m_pVkSurface = VK_NULL_HANDLE;
 		BackendConnectorHDRInfo m_HDRInfo{};
@@ -148,7 +151,7 @@ namespace gamescope
 
 		virtual std::shared_ptr<BackendBlob> CreateBackendBlob( const std::type_info &type, std::span<const uint8_t> data ) override;
 
-        virtual OwningRc<IBackendFb> ImportDmabufToBackend( wlr_buffer *pBuffer, wlr_dmabuf_attributes *pDmaBuf ) override;
+        virtual OwningRc<IBackendFb> ImportDmabufToBackend( wlr_dmabuf_attributes *pDmaBuf ) override;
 		virtual bool UsesModifiers() const override;
 		virtual std::span<const uint64_t> GetSupportedModifiers( uint32_t uDrmFormat ) const override;
 
@@ -208,7 +211,8 @@ namespace gamescope
 	// CSDLConnector
 	//////////////////
 
-	CSDLConnector::CSDLConnector()
+	CSDLConnector::CSDLConnector( CSDLBackend *pBackend )
+		: m_pBackend{ pBackend }
 	{
 	}
 
@@ -357,28 +361,23 @@ namespace gamescope
 
 	void CSDLConnector::SetCursorImage( std::shared_ptr<INestedHints::CursorInfo> info )
 	{
-		CSDLBackend *pBackend = static_cast<CSDLBackend *>( GetBackend() );
-		pBackend->SetCursorImage( std::move( info ) );
+		m_pBackend->SetCursorImage( std::move( info ) );
 	}
 	void CSDLConnector::SetRelativeMouseMode( bool bRelative )
 	{
-		CSDLBackend *pBackend = static_cast<CSDLBackend *>( GetBackend() );
-		pBackend->SetRelativeMouseMode( bRelative );
+		m_pBackend->SetRelativeMouseMode( bRelative );
 	}
 	void CSDLConnector::SetVisible( bool bVisible )
 	{
-		CSDLBackend *pBackend = static_cast<CSDLBackend *>( GetBackend() );
-		pBackend->SetVisible( bVisible );
+		m_pBackend->SetVisible( bVisible );
 	}
 	void CSDLConnector::SetTitle( std::shared_ptr<std::string> szTitle )
 	{
-		CSDLBackend *pBackend = static_cast<CSDLBackend *>( GetBackend() );
-		pBackend->SetTitle( std::move( szTitle ) );
+		m_pBackend->SetTitle( std::move( szTitle ) );
 	}
 	void CSDLConnector::SetIcon( std::shared_ptr<std::vector<uint32_t>> uIconPixels )
 	{
-		CSDLBackend *pBackend = static_cast<CSDLBackend *>( GetBackend() );
-		pBackend->SetIcon( std::move( uIconPixels ) );
+		m_pBackend->SetIcon( std::move( uIconPixels ) );
 	}
 
     void CSDLConnector::SetSelection( std::shared_ptr<std::string> szContents, GamescopeSelection eSelection )
@@ -394,7 +393,8 @@ namespace gamescope
 	////////////////
 
 	CSDLBackend::CSDLBackend()
-		: m_SDLThread{ [this](){ this->SDLThreadFunc(); } }
+		: m_Connector{ this }
+		, m_SDLThread{ [this](){ this->SDLThreadFunc(); } }
 	{
 	}
 
@@ -449,7 +449,7 @@ namespace gamescope
 		return std::make_shared<BackendBlob>( data );
 	}
 
-	OwningRc<IBackendFb> CSDLBackend::ImportDmabufToBackend( wlr_buffer *pBuffer, wlr_dmabuf_attributes *pDmaBuf )
+	OwningRc<IBackendFb> CSDLBackend::ImportDmabufToBackend( wlr_dmabuf_attributes *pDmaBuf )
 	{
 		return new CBaseBackendFb();
 	}

src/Backends/WaylandBackend.cpp
@@ -652,7 +652,7 @@ namespace gamescope
 
         virtual std::shared_ptr<BackendBlob> CreateBackendBlob( const std::type_info &type, std::span<const uint8_t> data ) override;
 
-        virtual OwningRc<IBackendFb> ImportDmabufToBackend( wlr_buffer *pBuffer, wlr_dmabuf_attributes *pDmaBuf ) override;
+        virtual OwningRc<IBackendFb> ImportDmabufToBackend( wlr_dmabuf_attributes *pDmaBuf ) override;
         virtual bool UsesModifiers() const override;
         virtual std::span<const uint64_t> GetSupportedModifiers( uint32_t uDrmFormat ) const override;
 
@@ -1625,7 +1625,7 @@ namespace gamescope
 
     void CWaylandPlane::Present( const FrameInfo_t::Layer_t *pLayer )
     {
-        CWaylandFb *pWaylandFb = pLayer && pLayer->tex != nullptr ? static_cast<CWaylandFb*>( pLayer->tex->GetBackendFb() ) : nullptr;
+        CWaylandFb *pWaylandFb = pLayer && pLayer->tex != nullptr ? static_cast<CWaylandFb*>( pLayer->tex->GetBackendFb()->Unwrap() ) : nullptr;
         wl_buffer *pBuffer = pWaylandFb ? pWaylandFb->GetHostBuffer() : nullptr;
 
         if ( pBuffer )
@@ -2064,7 +2064,7 @@ namespace gamescope
         {
             return false;
         }
-
+        
         if ( !wlsession_init() )
         {
             xdg_log.errorf( "Failed to initialize Wayland session" );
@@ -2076,6 +2076,8 @@ namespace gamescope
             xdg_log.errorf( "Failed to initialize input thread" );
             return false;
         }
+
+        xdg_log.infof( "Initted Wayland backend" );
 
         return true;
     }
@@ -2107,6 +2109,8 @@ namespace gamescope
 
         m_pDefaultCursorInfo = GetX11HostCursor();
         m_pDefaultCursorSurface = CursorInfoToSurface( m_pDefaultCursorInfo );
+
+        xdg_log.infof( "Post-Initted Wayland backend" );
 
         return true;
     }
@@ -2198,7 +2202,7 @@ namespace gamescope
         return std::make_shared<BackendBlob>( data );
     }
 
-    OwningRc<IBackendFb> CWaylandBackend::ImportDmabufToBackend( wlr_buffer *pClientBuffer, wlr_dmabuf_attributes *pDmaBuf )
+    OwningRc<IBackendFb> CWaylandBackend::ImportDmabufToBackend( wlr_dmabuf_attributes *pDmaBuf )
     {
         zwp_linux_buffer_params_v1 *pBufferParams = zwp_linux_dmabuf_v1_create_params( m_pLinuxDmabuf );
         if ( !pBufferParams )

src/backend.cpp
@@ -1,4 +1,5 @@
 #include "backend.h"
+#include "Backends/DeferredBackend.h"
 #include "vblankmanager.hpp"
 #include "convar.h"
 #include "wlserver.hpp"
@@ -9,6 +10,8 @@
 
 extern void sleep_until_nanos(uint64_t nanos);
 extern bool env_to_bool(const char *env);
+
+extern bool g_bAllowDeferredBackend;
 
 namespace gamescope
 {
@@ -38,9 +41,22 @@ namespace gamescope
             s_pBackend = pBackend;
             if ( !s_pBackend->Init() )
             {
-                delete s_pBackend;
-                s_pBackend = nullptr;
-                return false;
+                if ( g_bAllowDeferredBackend )
+                {
+                    s_pBackend = new CDeferredBackend( pBackend );
+                    if ( !s_pBackend->Init() )
+                    {
+                        delete s_pBackend;
+                        s_pBackend = nullptr;
+                        return false;
+                    }
+                }
+                else
+                {
+                    delete s_pBackend;
+                    s_pBackend = nullptr;
+                    return false;
+                }
             }
         }
 

src/backend.h
@@ -256,6 +256,8 @@ namespace gamescope
     public:
         virtual void SetBuffer( wlr_buffer *pClientBuffer ) = 0;
         virtual void SetReleasePoint( std::shared_ptr<CReleaseTimelinePoint> pReleasePoint ) = 0;
+
+        virtual IBackendFb *Unwrap() = 0;
     };
 
     class IBackendPlane
@@ -275,6 +277,8 @@ namespace gamescope
 
         void SetBuffer( wlr_buffer *pClientBuffer ) override;
         void SetReleasePoint( std::shared_ptr<CReleaseTimelinePoint> pReleasePoint ) override;
+
+        virtual IBackendFb *Unwrap() override { return this; };
 
     private:
         wlr_buffer *m_pClientBuffer = nullptr;
@@ -311,7 +315,7 @@ namespace gamescope
         //
         // shared_ptr owns the structure.
         // Rc manages acquire/release of buffer to/from client while imported.
-        virtual OwningRc<IBackendFb> ImportDmabufToBackend( wlr_buffer *pBuffer, wlr_dmabuf_attributes *pDmaBuf ) = 0;
+        virtual OwningRc<IBackendFb> ImportDmabufToBackend( wlr_dmabuf_attributes *pDmaBuf ) = 0;
 
         virtual bool UsesModifiers() const = 0;
         virtual std::span<const uint64_t> GetSupportedModifiers( uint32_t uDrmFormat ) const = 0;
@@ -366,6 +370,8 @@ namespace gamescope
         virtual bool SupportsVROverlayForwarding() = 0;
         virtual void ForwardFramebuffer( std::shared_ptr<IBackendPlane> &pPlane, IBackendFb *pFramebuffer, const void *pData ) = 0;
 
+        virtual bool NewlyInitted() = 0;
+
         static IBackend *Get();
         template <typename T>
         static bool Set();
@@ -398,6 +404,8 @@ namespace gamescope
 
         virtual bool SupportsVROverlayForwarding() override { return false; }
         virtual void ForwardFramebuffer( std::shared_ptr<IBackendPlane> &pPlane, IBackendFb *pFramebuffer, const void *pData ) override {}
+
+        virtual bool NewlyInitted() override { return false; }
     };
 
     // This is a blob of data that may be associated with

commit b98fcebe93532c52543e3aade60d8c1a41a8fae2

DRMBackend: Fix NULL dereference in drm_prepare_liftoff()
Fix gamescope crashing with a NULL dereference in drm_prepare_liftoff().

Fixes: 8f82d5153840 ("backend: Add support for deferred backends")
Signed-off-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>

src/Backends/DRMBackend.cpp
@@ -2558,7 +2558,7 @@ drm_prepare_liftoff( struct drm_t *drm, const struct FrameInfo_t *frameInfo, boo
 		if ( i < frameInfo->layerCount )
 		{
 			const FrameInfo_t::Layer_t *pLayer = &frameInfo->layers[ i ];
-			gamescope::CDRMFb *pDrmFb = static_cast<gamescope::CDRMFb *>( pLayer->tex ? pLayer->tex->GetBackendFb()->Unwrap() : nullptr );
+			gamescope::CDRMFb *pDrmFb = static_cast<gamescope::CDRMFb *>( (pLayer->tex && pLayer->tex->GetBackendFb()) ? pLayer->tex->GetBackendFb()->Unwrap() : nullptr );
 
 			if ( pDrmFb == nullptr )
 			{