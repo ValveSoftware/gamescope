1951a575569404ea65941a71322fe7855e9c944b
Toria <ninetailedtori@uwu.gal>
2026-05-11 21:45:13 +0100

    Move xkb_keymap over to WaylandBackend.cpp!
    Signed-off-by: Toria <ninetailedtori@uwu.gal>

--- src/Backends/WaylandBackend.cpp
+++ src/Backends/WaylandBackend.cpp
@@ -11,6 +11,7 @@
 #include "waitable.h"
 #include "Utils/TempFiles.h"
 
+#include <condition_variable>
 #include <cstring>
 #include <unordered_map>
 #include <unordered_set>
@@ -530,6 +531,8 @@ namespace gamescope
 
         void SetRelativePointer( bool bRelative );
 
+        ::xkb_keymap *Wayland_GetParentKeymap() const;
+
     private:
 
         void HandleKey( uint32_t uKey, bool bPressed );
@@ -556,8 +559,8 @@ namespace gamescope
 
         uint32_t m_uFakeTimestamp = 0;
 
-        xkb_context *m_pXkbContext = nullptr;
-        xkb_keymap *m_pXkbKeymap = nullptr;
+        xkb_context *m_pXkbContext        = nullptr;
+        ::xkb_keymap *m_pXkbKeymap = nullptr;
 
         uint32_t m_uKeyModifiers = 0;
         uint32_t m_uModMask[ GAMESCOPE_WAYLAND_MOD_COUNT ];
@@ -635,6 +638,7 @@ namespace gamescope
         .modifiers     = WAYLAND_USERDATA_TO_THIS( CWaylandInputThread, Wayland_Keyboard_Modifiers ),
         .repeat_info   = WAYLAND_USERDATA_TO_THIS( CWaylandInputThread, Wayland_Keyboard_RepeatInfo ),
     };
+
     const zwp_relative_pointer_v1_listener CWaylandInputThread::s_RelativePointerListener =
     {
         .relative_motion = WAYLAND_USERDATA_TO_THIS( CWaylandInputThread, Wayland_RelativePointer_RelativeMotion ),
@@ -685,6 +689,8 @@ namespace gamescope
 
         virtual bool UsesVirtualConnectors() override;
         virtual std::shared_ptr<IBackendConnector> CreateVirtualConnector( uint64_t ulVirtualConnectorKey ) override;
+    	
+    	::xkb_keymap *GetParentKeymap() const override;
     protected:
         virtual void OnBackendBlobDestroyed( BackendBlob *pBlob ) override;
 
@@ -2359,6 +2365,11 @@ namespace gamescope
         return pConnector;
     }
 
+    ::xkb_keymap *CWaylandBackend::GetParentKeymap() const
+    {
+        return m_InputThread.Wayland_GetParentKeymap();
+    }
+
     ///////////////////
     // INestedHints
     ///////////////////
@@ -2927,6 +2938,10 @@ namespace gamescope
         }
     }
 
+    ::xkb_keymap *CWaylandInputThread::Wayland_GetParentKeymap() const{
+		return m_pXkbKeymap;
+	}
+
     void CWaylandInputThread::HandleKey( uint32_t uKey, bool bPressed )
     {
         if ( m_uKeyModifiers & m_uModMask[ GAMESCOPE_WAYLAND_MOD_META ] )
@@ -3219,7 +3234,12 @@ namespace gamescope
         }
         defer( munmap( pMap, uSize ) );
 
-        xkb_keymap *pKeymap = xkb_keymap_new_from_string( m_pXkbContext, pMap, XKB_KEYMAP_FORMAT_TEXT_V1, XKB_KEYMAP_COMPILE_NO_FLAGS );
+        ::xkb_keymap *pKeymap = xkb_keymap_new_from_string(
+	        m_pXkbContext,
+	        pMap,
+	        XKB_KEYMAP_FORMAT_TEXT_V1,
+	        XKB_KEYMAP_COMPILE_NO_FLAGS
+        );
         if ( !pKeymap )
         {
             xdg_log.errorf( "Failed to create xkb_keymap" );
